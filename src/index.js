6b6780ccbaf2c21d7fd863d8fc825e59bbd7ce4c
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-24 12:03:45 -0400

    zach revs 1200

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
-  // debugger
+  
   
   fetch("http://localhost:3000/toys")
     .then(res => res.json())
@@ -14,19 +14,47 @@ document.addEventListener("DOMContentLoaded", () => {
         
         let cardDiv = document.createElement("div")
             cardDiv.classList = "card"
+
         let h2Tag = document.createElement("h2")
+            h2Tag.innerText = toy.name
+
         let imgTag = document.createElement("img")
             imgTag.src = toy.image
+
         let likesTag = document.createElement("p")    
             likesTag.innerText = toy.likes
 
+        let likesButton = document.createElement("button")
+            likesButton.classList = "like-btn"
+
+        cardDiv.append(h2Tag)
+        cardDiv.append(imgTag)
+        cardDiv.append(likesTag)
+        cardDiv.append(likesButton)
 
         toyCollectionDiv.append(cardDiv)
 
       })
-      // cardDiv.innerText = toy
     })
   
+  toyFormContainer.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    let toyNameUserInput = event.target.name.value
+    let imageUrlUserInput = event.target.image.value
+
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        "name": toyNameUserInput,
+        "image": imageUrlUserInput,
+        "likes": 0
+      })
+    })
+  })
   
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
