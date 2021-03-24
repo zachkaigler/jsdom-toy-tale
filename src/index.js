7a3722f65e9dd65b7d031e47d6f15eb3d17a4c0b
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-24 11:05:14 -0400

    Zach code 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 let addToy = false;
 
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  // debugger
+  
+  fetch("http://localhost:3000/toys")
+    .then(res => res.json())
+    .then(function(toysArray) {
+      toysArray.forEach(function (toy) {
+        let toyCollectionDiv = document.querySelector("div#toy-collection")
+        
+        let cardDiv = document.createElement("div")
+            cardDiv.classList = "card"
+            
+        toyCollectionDiv.append(cardDiv)
+      })
+      // cardDiv.innerText = toy
+    })
+  
+  
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
